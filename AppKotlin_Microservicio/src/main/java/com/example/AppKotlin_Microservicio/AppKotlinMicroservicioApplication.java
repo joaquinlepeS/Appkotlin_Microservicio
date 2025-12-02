326234f888dc9eeed694edd0bfb4af4636a7c1b6
joaquinlepeS <joa.lepe@duocuc.cl>
2025-12-02 10:51:30 -0300

    Add files via upload

---
+++ AppKotlin_Microservicio/src/main/java/com/example/AppKotlin_Microservicio/AppKotlinMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.example.AppKotlin_Microservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppKotlinMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppKotlinMicroservicioApplication.class, args);
+	}
+
+}
---
+++ AppKotlin_Microservicio/src/test/java/com/example/AppKotlin_Microservicio/AppKotlinMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AppKotlin_Microservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppKotlinMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
